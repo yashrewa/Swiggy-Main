f066e56fc86d63ebdf4aa6bab08f23cd4affa7b1
yashrewa <yashrewa00@gmail.com>
2023-10-13 10:41:34 +0530

    fix cors error

--- server/Dist/app.js
+++ server/Dist/app.js
@@ -5,18 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const fs_1 = __importDefault(require("fs"));
+const http_1 = __importDefault(require("http"));
+const https_1 = __importDefault(require("https"));
 const cors_1 = __importDefault(require("cors"));
 const admin_1 = __importDefault(require("./routes/admin"));
 const user_1 = __importDefault(require("./routes/user"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
 app.use((0, cors_1.default)({
-    origin: 'https://swiggy-main.vercel.app',
+    origin: 'https://swiggy-main-3acb0bt4v-yashs-projects-dfb34b1e.vercel.app/'
 }));
-// app.use(express.json())
+app.use(express_1.default.json());
 app.use('/admin', admin_1.default);
 app.use('/user', user_1.default);
-mongoose_1.default.connect(`mongodb+srv://yashrewa00:21Savage@cluster0.fngj58u.mongodb.net/swiggy?retryWrites=true&w=majority`);
+let options = {
+    key: fs_1.default.readFileSync('./newcerts/ubereats.key'),
+    cert: fs_1.default.readFileSync('./newcerts/ubereats.crt')
+};
+console.log(options);
+mongoose_1.default.connect(`mongodb+srv://yashrewa00:21Savage@cluster0.3z81dfx.mongodb.net/Swiggy?retryWrites=true&w=majority`);
 // mongoose.connect(`${process.env.MONGODB_CONNECT_URI}`)
-// const PORT = process.env.PORT;
-app.listen(3000);
+http_1.default.createServer(app).listen(80);
+https_1.default.createServer(options, app).listen(443);
--- server/app.ts
+++ server/app.ts
@@ -1,25 +1,22 @@
 import express, {Request, Response} from 'express';
 import mongoose from 'mongoose'
+import fs from 'fs'
 import cors from 'cors';
 import adminRoutes from './routes/admin'
 import userRoutes from './routes/user'
 const app = express()
 
-app.use(cors())
-
 app.use(cors({
-    origin: 'https://swiggy-main.vercel.app',
-    
+    origin: 'https://swiggy-main-3acb0bt4v-yashs-projects-dfb34b1e.vercel.app/'
 }))
-// app.use(express.json())
+app.use(express.json())
 
 app.use('/admin', adminRoutes)
 app.use('/user', userRoutes)
 
-mongoose.connect(`mongodb+srv://yashrewa00:21Savage@cluster0.fngj58u.mongodb.net/swiggy?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://yashrewa00:21Savage@cluster0.3z81dfx.mongodb.net/Swiggy?retryWrites=true&w=majority`)
 
 // mongoose.connect(`${process.env.MONGODB_CONNECT_URI}`)
 
-// const PORT = process.env.PORT;
 
 app.listen(3000)
