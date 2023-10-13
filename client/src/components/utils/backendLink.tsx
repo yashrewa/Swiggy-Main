9534bbfa4045e4d13a2757004e98ac46118faaf4
yashrewa <yashrewa00@gmail.com>
2023-10-13 19:55:33 +0530

    fixed port issue 7

--- client/src/components/utils/backendLink.tsx
+++ client/src/components/utils/backendLink.tsx
@@ -1,2 +1,5 @@
-export const backendLink = "https://golden-operator-401018.as.r.appspot.com/"
+// export const backendLink = "https://golden-operator-401018.as.r.appspot.com/"
+export const backendLink = import.meta.env.VITE_BACKEND_URL;
+// export const backendLink = procces.env.BACKEND_URL
+
 // export const backendLink = "http://localhost:3000"
