f753d25c1956171f8dbe9ed8ca2d81aab0cc68a6
yashrewa <yashrewa00@gmail.com>
2024-02-23 14:05:22 +0530

    backend ported from somewhere to somewhere else due to cost issues

--- client/src/components/utils/backendLink.tsx
+++ client/src/components/utils/backendLink.tsx
@@ -1,6 +1,6 @@
 // export const backendLink = "https://golden-operator-401018.as.r.appspot.com/"
 // export const backendLink = import.meta.env.VITE_BACKEND_URL;
-export const backendLink = 'https://swiggy-main.onrender.com/';
+export const backendLink = 'https://swiggy-main.onrender.com';
 // export const backendLink = procces.env.BACKEND_URL
 
 // export const backendLink = "http://localhost:3000"
